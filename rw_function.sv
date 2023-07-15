ca0759f35ae21cdff2572a8006001cf4e00821b5
gleamsss <100515910+gleamsss@users.noreply.github.com>
2023-07-15 12:07:46 +0800

    Add files via upload

--- rw_function.sv
+++ rw_function.sv
@@ -20,40 +20,57 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 class read_write_txt;
-function void write_txt(string file_name,logic [8-1:0]write_array[0:4-1]);
+integer WIDTH,DEPTH;
+int my_array_buffer[];
+function new(integer width,integer depth);
+    this.WIDTH=width;
+    this.DEPTH=depth;
+    my_array_buffer=new[this.DEPTH];
+    $display("WIDTH IS %d,DEPTH IS %d",this.WIDTH,this.DEPTH);
+endfunction
+
+function void write_txt(string file_name,const ref int write_array[]);
+    for (integer i= 0;i<this.DEPTH ;i=i+1) begin
+        my_array_buffer[i]=0;
+        my_array_buffer[i]=write_array[i];
+    end
     //write data to file
-    $writememh(file_name, write_array,0);
+    $writememh(file_name, my_array_buffer,0);
     $display("write txt finish!");
 endfunction
 
-function automatic logic[7:0]read_txt(string file_name,integer addr);
-    logic[7:0] get_str[0:3];
+function void read_txt(string file_name);
     //read data from file
-    $readmemh(file_name,get_str,0,3);
+    for (integer i= 0;i<this.DEPTH ;i=i+1) begin
+        my_array_buffer[i]=0;
+    end
+    $readmemh(file_name,my_array_buffer,0,3);
     $display("read txt finish!");
-    return get_str[addr];
 endfunction
-
-
 endclass //read_write_txt
 
 module rw_function;
-read_write_txt rw1;
-logic [7:0] my_array[0:3];
+read_write_txt rw1=new(width,depth);
+integer width=8;
+integer depth=40;
+int my_array[]=new[depth];
 string file_name="D:/datas/IC_verification_proj/phase_learn/sample.txt";
 logic[7:0] ans;
 initial begin
-for (int i = 0; i < 4; i = i + 1) begin
+for (int i = 0; i < depth; i = i + 1) begin
     my_array[i]=i+10;
     $display(my_array[i]);
 end
 
 rw1.write_txt(file_name,my_array); 
+for (integer i= 0;i<depth ;i=i+1) begin
+        $display("my_array_buffer[%d] is %d",i,rw1.my_array_buffer[i]);
+    end
 
-for (int i =0 ; i<4;i=i+1 ) begin
-    ans=rw1.read_txt(file_name,i);
-    $display("%2x",ans);
-end
+// for (int i =0 ; i<4;i=i+1 ) begin
+//     ans=rw1.read_txt(file_name,i);
+//     $display("%2x",ans);
+// end
 
  
 
